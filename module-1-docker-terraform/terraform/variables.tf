da131d305482f4133606c43c15b5025d64e12ddd
Jasper Alblas <just_jasper@hotmail.com>
2025-05-22 20:46:26 +0200

    edited terraform variable fields

--- module-1-docker-terraform/terraform/variables.tf
+++ module-1-docker-terraform/terraform/variables.tf
@@ -1,10 +1,7 @@
 variable "credentials" {
   description = "My Credentials"
   default     = "./key/dtc-de-course-460512-e3d58a03a3eb.json"
-  #ex: if you have a directory where this file is called keys with your service account json file
-  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
-
 
 variable "project" {
   description = "Project"
@@ -23,12 +20,12 @@ variable "location" {
 
 variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
-  default     = "demo_dataset"
+  default     = "de_bootcamp_bq"
 }
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
-  default     = "terraform-demo-terra-bucket"
+  default     = "de-bootcamp-bucket"
 }
 
 variable "gcs_storage_class" {
