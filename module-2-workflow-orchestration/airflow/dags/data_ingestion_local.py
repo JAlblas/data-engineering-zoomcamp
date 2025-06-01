c77fc6bbc96b33005858a8d6f91a71cfaccb2d67
Jasper Alblas <just_jasper@hotmail.com>
2025-06-01 11:56:31 +0200

    created first dag

---
+++ module-2-workflow-orchestration/airflow/dags/data_ingestion_local.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+
+local_workflow = DAG(
+    "LocalIngetionDAG",
+    schedule="0 6 2 * *",
+    start_date=datetime(2021, 1, 1)
+)
+
+with local_workflow:
+    wget_task = BashOperator(
+        task_id='wget',
+        bash_command='echo "hello world"'
+    )
+
+    ingest_task = BashOperator(
+        task_id='ingest',
+        bash_command='echo "hello world"'
+    )
+
+    wget >> ingest_task
