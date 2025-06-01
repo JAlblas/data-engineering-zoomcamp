ae314d4cb1a1a7ae3b8c78c3e09f830a89231504
Jasper Alblas <just_jasper@hotmail.com>
2025-06-01 20:53:28 +0200

    dag now fetches correct data

--- module-2-workflow-orchestration/airflow/dags/data_ingestion_local.py
+++ module-2-workflow-orchestration/airflow/dags/data_ingestion_local.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 
 from airflow import DAG
@@ -5,22 +6,28 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
 local_workflow = DAG(
-    "LocalIngetionDAG",
+    "LocalIngestionDAG",
     schedule="0 6 2 * *",
     start_date=datetime(2021, 1, 1)
 )
 
+URL_PREFIX = 'https://d37ci6vzurychx.cloudfront.net/trip-data' 
+URL_TEMPLATE = URL_PREFIX + '/yellow_tripdata_{{ data_interval_start.strftime(\'%Y-%m\') }}.parquet'
+OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/output_{{ data_interval_start.strftime(\'%Y-%m\') }}.parquet'
+#TABLE_NAME_TEMPLATE = 'yellow_taxi_{{ execution_date.strftime(\'%Y_%m\') }}'
+
 with local_workflow:
     wget_task = BashOperator(
         task_id='wget',
-        bash_command='echo "hello world"'
+         bash_command=f'curl -sSL {URL_TEMPLATE} > {OUTPUT_FILE_TEMPLATE}'
     )
 
     ingest_task = BashOperator(
         task_id='ingest',
-        bash_command='echo "hello world"'
+        bash_command=f'ls {AIRFLOW_HOME} '
     )
 
     wget_task >> ingest_task
