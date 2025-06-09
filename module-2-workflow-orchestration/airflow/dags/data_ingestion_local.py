b5b65e908fd56701e062b8bb806f3979ccb1026a
Jasper Alblas <just_jasper@hotmail.com>
2025-06-09 19:49:06 +0200

    Edited DAG to read parquet file instead of csv

--- module-2-workflow-orchestration/airflow/dags/data_ingestion_local.py
+++ module-2-workflow-orchestration/airflow/dags/data_ingestion_local.py
@@ -10,6 +10,12 @@ from ingest_script import ingest_callable
 
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
+#PG_HOST = os.getenv('PG_HOST')
+#PG_USER = os.getenv('PG_USER')
+#PG_PASSWORD = os.getenv('PG_PASSWORD')
+#PG_PORT = os.getenv('PG_PORT')
+#PG_DATABASE = os.getenv('PG_DATABASE')
+
 local_workflow = DAG(
     "LocalIngestionDAG",
     schedule="0 6 2 * *",
@@ -19,7 +25,13 @@ local_workflow = DAG(
 URL_PREFIX = 'https://d37ci6vzurychx.cloudfront.net/trip-data' 
 URL_TEMPLATE = URL_PREFIX + '/yellow_tripdata_{{ data_interval_start.strftime(\'%Y-%m\') }}.parquet'
 OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + '/output_{{ data_interval_start.strftime(\'%Y-%m\') }}.parquet'
-#TABLE_NAME_TEMPLATE = 'yellow_taxi_{{ execution_date.strftime(\'%Y_%m\') }}'
+TABLE_NAME_TEMPLATE = 'yellow_taxi_{{ data_interval_start.strftime(\'%Y_%m\') }}'
+
+PG_USER = os.getenv('PG_USER')
+PG_PASSWORD = os.getenv('PG_PASSWORD')
+PG_HOST = os.getenv('PG_HOST')
+PG_PORT = os.getenv('PG_PORT')
+PG_DATABASE = os.getenv('PG_DATABASE')
 
 with local_workflow:
     wget_task = BashOperator(
@@ -28,10 +40,17 @@ with local_workflow:
     )
 
     ingest_task = PythonOperator(
-        task_id='ingest_callable',
-        op_args={
-
-        }
+        task_id="ingest",
+        python_callable=ingest_callable,
+        op_kwargs=dict(
+            user=PG_USER,
+            password=PG_PASSWORD,
+            host=PG_HOST,
+            port=PG_PORT,
+            db=PG_DATABASE,
+            table_name=TABLE_NAME_TEMPLATE,
+            csv_file=OUTPUT_FILE_TEMPLATE
+        ),
     )
 
     wget_task >> ingest_task
--- module-2-workflow-orchestration/airflow/dags/ingest_script.py
+++ module-2-workflow-orchestration/airflow/dags/ingest_script.py
@@ -6,8 +6,11 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def ingest_callable(user, password, host, port, db, table_name, csv_file, execution_date):
-    print(table_name, csv_file, execution_date)
+def ingest_callable(user, password, host, port, db, table_name, csv_file,  **context):
+    print("Starting ingest_callable", flush=True)
+
+    execution_date = context.get('data_interval_start')
+    print(table_name, csv_file, execution_date, user, password, flush=True)
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     engine.connect()
@@ -15,9 +18,7 @@ def ingest_callable(user, password, host, port, db, table_name, csv_file, execut
     print('connection established successfully, inserting data...')
 
     t_start = time()
-    df_iter = pd.read_csv(csv_file, iterator=True, chunksize=100000)
-
-    df = next(df_iter)
+    df = pd.read_parquet(csv_file)
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
@@ -27,22 +28,4 @@ def ingest_callable(user, password, host, port, db, table_name, csv_file, execut
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
     t_end = time()
-    print('inserted the first chunk, took %.3f second' % (t_end - t_start))
-
-    while True: 
-        t_start = time()
-
-        try:
-            df = next(df_iter)
-        except StopIteration:
-            print("completed")
-            break
-
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-        df.to_sql(name=table_name, con=engine, if_exists='append')
-
-        t_end = time()
-
-        print('inserted another chunk, took %.3f second' % (t_end - t_start))
+    print('inserted data, took %.3f second' % (t_end - t_start))
